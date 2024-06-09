5a29cc5703762853136eab813dff4626976d4c04
Niklas Sombert <niklas@ytvwld.de>
2024-06-09 15:02:33 +0200

    Boot to gdm

--- build.sh
+++ build.sh
@@ -4,17 +4,24 @@ set -eu
 # create and export the rootfs
 cd rootfs && docker build -t canebo . && cd ..
 docker run -d --name canebo canebo
-rm -rf build/rootfs
-mkdir build/rootfs
-docker cp canebo:/ build/rootfs
+docker export -o build/rootfs.tar canebo
 docker stop canebo
 docker rm canebo
 # docker rmi canebo
+rm -rf build/rootfs
+mkdir build/rootfs
+# we need to use tar to extract because docker cp doesn't use C's IO functions,
+# so fakeroot has no effect there
+fakeroot -s build/fakeroot tar -C build/rootfs -xf build/rootfs.tar
+rm build/rootfs.tar
 
 # get the kernel and throw away the rest of /boot
 cp --copy-contents build/rootfs/boot/vmlinuz-* build/kernel
 rm -rf build/rootfs/boot
 
+# let systemd know that this isn't docker
+rm build/rootfs/.dockerenv
+
 # create the initramfs
-cd build/rootfs && find . | cpio -o --format=newc | gzip -9 >../initramfs && cd ..
+cd build/rootfs && find . | fakeroot -i ../fakeroot cpio -o --format=newc | xz -9 --format=xz --check=crc32 - > ../initramfs && cd ..
 rm -rf build/rootfs
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 -kernel build/kernel -initrd build/initramfs -m 4096 -serial stdio -append console=ttyS0
+qemu-system-x86_64 -kernel build/kernel -initrd build/initramfs -m 6144 -serial stdio -machine pc,accel=kvm
