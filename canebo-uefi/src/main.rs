ffd3e3af2b08148143f5f4e85dc08f095d508622
Niklas Sombert <niklas@ytvwld.de>
2024-09-20 18:39:12 +0200

    Add lvgl

--- canebo-uefi/src/main.rs
+++ canebo-uefi/src/main.rs
@@ -1,24 +1,38 @@
 #![no_std]
 #![no_main]
 
-use boot::{find_handles, open_protocol_exclusive};
+use embedded_graphics::draw_target::DrawTarget;
+use lvgl::Display;
+use lvgl::DrawBuffer;
 use uefi::prelude::*;
+use uefi::boot::{find_handles, open_protocol_exclusive};
 use uefi::Result;
 use uefi::proto::console::gop::GraphicsOutput;
 use uefi_graphics2::UefiDisplay;
 
-fn initialize_graphics() -> Result<UefiDisplay> {
+fn initialize_graphics() -> Result<Display> {
+    const RES_4K: usize = 3840 * 2160;
+
     // just open the first screen
     let mut gop = open_protocol_exclusive::<GraphicsOutput>(
         find_handles::<GraphicsOutput>()?
             .into_iter().next().ok_or(Status::UNSUPPORTED)?
     )?;
     let mode = gop.current_mode_info();
+    let (xres, yres) = mode.resolution();
+    assert!(xres * yres < RES_4K);
     let buffer = gop.frame_buffer();
-    Ok(
-        UefiDisplay::new(buffer, mode)
-            .map_err(|_err| Status::UNSUPPORTED)?
-    )
+    let mut uefi_display = UefiDisplay::new(buffer, mode)
+            .map_err(|_err| Status::UNSUPPORTED)?;
+    lvgl::init();
+    
+    let buffer = DrawBuffer::<RES_4K>::default();
+    Ok(Display::register(
+        buffer,
+        yres.try_into().unwrap(),
+        xres.try_into().unwrap(),
+        |refresh| uefi_display.draw_iter(refresh.as_pixels()).unwrap(),
+    ).map_err(|_err| Status::UNSUPPORTED)?)
 }
 
 #[entry]
